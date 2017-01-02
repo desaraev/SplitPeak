45a1e6d57b1d5583b379ef4c4aa32f1ef4fee58c
Desarae <desaraev@gmail.com>
2017-01-01 19:27:00 -0600

    prepros setup

---
+++ css/config.rb
@@ -0,0 +1,22 @@
+# Set this to the root of your project when deployed:
+# http_path = "https://rawgit.com/UltimateHearingCenter/UHC/"
+
+# Set the images directory relative to your http_path or change
+# the location of the images themselves using http_images_path:
+http_images_dir = "SplitPeak/css/images/"
+
+# Production Assets URL
+http_images_path = "SplitPeak/css/images/"
+
+css_dir = "min"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :compressed
+environment = :development
+
+relative_assets = false
+
+line_comments = false
+color_output = false
